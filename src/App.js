2ddc1668371f684692a4a913c596317e46c6784d
Vinay <vinay.d.kumar@macys.com>
2020-05-29 11:08:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Lets Learn React Testing</h1>
-    </div>
-  );
+// function App1() {
+
+//   const [count,setCount]=useState(0)
+
+//   return (
+//     <div className="App" data-test="component-app">
+//       <h1 data-test="counter-display" >The Count is {count}</h1>
+//       <button onClick={()=>setCount(count+1)} data-test="increment-button" >
+//         Increment Counter
+//         </button>
+//       <button onClick={()=>setCount(0)} data-test="increment-button">
+//         Reset Counter
+//         </button>
+//     </div>
+//   );
+// }
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      count: 0,
+      error:false
+     };
+  }
+
+  decrement=()=>{
+    if(this.state.count ===0){
+this.setState({error:true})
+    }
+    else{
+      this.setState({count:this.state.count-1})
+    }
+    
+  }
+
+  increment=()=>{
+    if(this.state.error){
+      this.setState({error:false})
+          }
+    this.setState({count:this.state.count+1})
+  }
+  render(){
+    const errorClass = this.state.error ? '' : 'hidden';
+    return(
+<div data-test="component-app">
+<div data-test="error-message" className={`error ${errorClass}`}>
+        The counter cannot go below 0
+      </div>
+  <h1 data-test="counter-display" >Count Value:{this.state.count} </h1>
+  <button data-test="increment-button" onClick={this.increment} >
+        Increment Counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrement} >
+        Decrement Counter
+        </button>
+</div>
+    );
+  }
 }
 
 export default App;
