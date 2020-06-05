ffe08cc75b26ff1a5d29222a1cbbb8c9a99a0fcf
Vinay <vinay.d.kumar@macys.com>
2020-06-05 10:47:23 +0530

    Input Test cases

--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Input({secretWord}){
-    return(
-        <div data-test='component-input'/>
-    )
+function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          className="mb-2 mx-sm-3"
+          data-test="input-box"
+          placeholder="Enter Guess"
+          type="text"
+          value={currentGuess}
+          onChange={(event)=>setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(event) => {
+              event.preventDefault();
+            setCurrentGuess('');
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-Input.propTypes={
-    secretWord:PropTypes.string.isRequired
-}
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
+};
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Input from './Input';
+import React from "react";
+import { shallow } from "enzyme";
+import Input from "./Input";
 
-import { findByTestAttr,checkProps } from '../test/testUtils';
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
-const setup = (secretWord='Party')=>{
-const wrapper= shallow(<Input secretWord={secretWord}/>)
-return wrapper;
-}
+const setup = (secretWord = "Party") => {
+  const wrapper = shallow(<Input secretWord={secretWord} />);
+  return wrapper;
+};
 
-describe('Input Component',()=>{
+describe("Input Component", () => {
+  const defaultProps = {
+    secretWord: "Train"
+  };
 
-    const defaultProps={
-        secretWord:'Test'
-    }
+  it("Checking Prop types", () => {
+    checkProps(Input, defaultProps);
+  });
+  it("render without any error", () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, "component-input");
+    expect(appComponent.length).toBe(1);
+  });
 
-    it ('Checking Prop types',()=>{
-        checkProps(Input,defaultProps);
+  describe("Mocking setState Function", () => {
 
-    })
-    it('render without any error',()=>{
-        const wrapper = setup();
-        const appComponent = findByTestAttr(wrapper,'component-input');
-        expect(appComponent.length).toBe(1);
-        });
-})
+    let mockSetCurrentGuess = jest.fn();
+    let wrapper;
 
+    beforeEach(() => {
+      mockSetCurrentGuess.mockClear();
+      React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+      wrapper = setup();
+    });
+
+    it("state update on input value change", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      const mockEvent = { target: { value: "train" } };
+      inputBox.simulate("change", mockEvent);
+      expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+    });
+
+    it("Field is cleared upon submit button click", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      submitButton.simulate("click",{preventDefault(){}});
+      expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+    });
+  });
+});
